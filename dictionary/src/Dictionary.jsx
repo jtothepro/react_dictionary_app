a2197f7e6302f4f6c1d9db6cb017e5963c179909
jtothepro <168333109+jtothepro@users.noreply.github.com>
2025-11-27 16:07:15 +0300

    Setup of results.jsx and meaning.jsx

--- dictionary/src/Dictionary.jsx
+++ dictionary/src/Dictionary.jsx
@@ -1,12 +1,16 @@
 import "./Dictionary.css";
 import { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -25,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
